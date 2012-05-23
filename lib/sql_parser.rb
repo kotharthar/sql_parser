57d6976255ee90ba98f586ef290174c7cdd0a1fe
Thar Htet <tharhtet@rivaledge.sg>
2012-05-23 13:20:44 +0800

    Parser for IN keyword syntax classess and Grammer rules are added.

--- lib/sql_parser.rb
+++ lib/sql_parser.rb
@@ -10,6 +10,12 @@ class SqlParser::ItemsNode < Treetop::Runtime::SyntaxNode
   end
 end
 
+class SqlParser::InListItemsNode < Treetop::Runtime::SyntaxNode
+  def values
+    inlist_items.values.unshift(inlist_item.value)
+  end
+end
+
 class SqlParser::ItemNode < Treetop::Runtime::SyntaxNode
   def values
     [value]
@@ -17,5 +23,25 @@ class SqlParser::ItemNode < Treetop::Runtime::SyntaxNode
   
   def value
     text_value.to_sym
+  end
+end
+
+class SqlParser::InListStringItemNode < Treetop::Runtime::SyntaxNode
+  def values
+    [value]
+  end
+  
+  def value
+    text_value[1..-2]
+  end
+end
+
+class SqlParser::InListNumberItemNode < Treetop::Runtime::SyntaxNode
+  def values
+    [value]
+  end
+  
+  def value
+    Integer(text_value)
   end
 end
